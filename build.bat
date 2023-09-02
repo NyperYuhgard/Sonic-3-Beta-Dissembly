834a40aaf744d1a327dcbc269374859714e80a35
NyperYuhgard <93950153+NyperYuhgard@users.noreply.github.com>
2023-09-02 16:52:41 -0500

    Zzz

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- S303111993.asm, s3built.bin
+asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p S303111993.asm, s3built.bin
 
 pause
