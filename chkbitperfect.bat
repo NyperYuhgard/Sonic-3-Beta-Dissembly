58365853a15d8d02d1ea6a717a2a7d52d811bed4
NyperYuhgard <93950153+NyperYuhgard@users.noreply.github.com>
2024-06-03 09:55:42 -0500

    CheckBitPerfect

---
+++ chkbitperfect.bat
@@ -0,0 +1,13 @@
+@echo OFF
+
+REM // build the ROM
+call build
+
+REM  // run fc against a Sonic 1 Rev 01 ROM
+echo -------------------------------------------------------------
+if exist s3built.bin ( fc /b s3built.bin Sonic 3 Proto.md
+) else echo Sega_TMSS.bin does not exist, probably due to an assembly error
+
+REM // if someone ran this from Windows Explorer, prevent the window from disappearing immediately
+pause
+
